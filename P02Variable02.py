9302b908275a7cf40f6bb7b609aacd376c9b4d90
Jonas Muniz <ev.jonasmuniz@gmail.com>
2020-03-20 23:46:44 -0300

    Declaring variables

---
+++ P02Variable02.py
@@ -0,0 +1,6 @@
+manager=input("Enter the name of the manager: ")
+employee=input("Enter the name of the employee: ")
+event=input("Enter the name of the event: ")
+value=float(input("Enter the amount to be refunded: "))
+
+print("I declare to Mr. " + manager + ", that the Mr. " + employee + " was at the event " + event + " and spent the amount U$ " + str(value) + " at the entrance.")
