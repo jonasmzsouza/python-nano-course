45933c050869f39979e8386376ad29ea26f01f95
Jonas Muniz <ev.jonasmuniz@gmail.com>
2020-03-21 00:08:04 -0300

    Inserting the condition of contagious disease

--- P03DecisionIf.py
+++ P03DecisionIf.py
@@ -1,6 +1,9 @@
 name=input("Enter the name: ")
 age=int(input("Enter the age: "))
+contagiousDisease=input("Suspected contagious disease? ").upper()
 if age >= 65:
-    print("The patient " + name + " has priority care.")
+    print("The patient " + name + " has priority service.")
+elif contagiousDisease == "YES":
+    print("The patient " + name + " must be taken to the reserved waiting room.")
 else:
-    print("The patient " + name + " hasn't priority care.")
+    print("The patient " + name + " hasn't priority service.")
