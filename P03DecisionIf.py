90ca8783f1cb05f71ec7607a0083d07c1577625e
Jonas Muniz <ev.jonasmuniz@gmail.com>
2020-03-21 00:18:34 -0300

    Decision with operator AND

--- P03DecisionIf.py
+++ P03DecisionIf.py
@@ -1,9 +1,13 @@
 name=input("Enter the name: ")
 age=int(input("Enter the age: "))
 contagiousDisease=input("Suspected contagious disease? ").upper()
-if age >= 65:
-    print("The patient " + name + " has priority service.")
-elif contagiousDisease == "YES":
-    print("The patient " + name + " must be taken to the reserved waiting room.")
+if age >= 65 and contagiousDisease == "YES":
+    print("Refer the patient " + name + " to the yellow room - With priority")
+if age < 65 and contagiousDisease == "YES":
+    print("Refer the patient " + name + " to the yellow room - Without priority")
+if age >= 65 and contagiousDisease == "NO":
+    print("Refer the patient " + name + " to the white room - With priority")
+if age < 65 and contagiousDisease == "NO":
+    print("Refer the patient " + name + " to the white room - Without priority")
 else:
-    print("The patient " + name + " hasn't priority service.")
+    print("Answer YES or NO to contagious disease")
