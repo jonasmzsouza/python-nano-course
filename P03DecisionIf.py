630aae920b0263f8bbd762d8c18256d0cec3ce9a
Jonas Muniz <ev.jonasmuniz@gmail.com>
2020-03-21 01:41:40 -0300

    Decision with more conditions

--- P03DecisionIf.py
+++ P03DecisionIf.py
@@ -1,19 +1,24 @@
 name=input("Enter the name: ")
 age=int(input("Enter the age: "))
+gender=input("Enter the gender of the patient: ").upper()
 contagiousDisease=input("Suspected contagious disease? ").upper()
-if age >= 65:
-    print("Patient with priority")
-    if contagiousDisease == "YES":
-        print("Refer the patient " + name + " to the yellow room")
-    elif contagiousDisease == "NO":
-        print("Refer the patient " + name + " to the white room")
-    else:
-        print("Answer YES or NO to contagious disease")
+
+if gender == "WOMAN" and age > 10:
+    pregnancy=input("Is the patient pregnant? ").upper()
 else:
-    print("Patient without priority")
-    if contagiousDisease == "YES":
-        print("Refer the patient " + name + " to the yellow room")
-    elif contagiousDisease == "NO":
-        print("Refer the patient " + name + " to the white room")
-    else:
-        print("Answer YES or NO to contagious disease")
+    pregnancy="NO"
+
+if contagiousDisease == "YES":
+    print("Refer the patient " + name + " to the yellow room")
+elif contagiousDisease == "NO" and age >= 65:
+    print("Refer the patient " + name + " to the white room - WITH priority.")
+elif contagiousDisease == "NO" and pregnancy == "YES" :
+    print("Refer the patient " + name + " to the white room - WITH priority.")
+elif contagiousDisease == "NO" and age < 65:
+    print("Refer the patient " + name + " to the white room - WITHOUT priority.")
+else:
+    print("Answer YES or NO to contagious disease")
+
+
+
+
