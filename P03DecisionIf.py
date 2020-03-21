2c78babdbfad3dd6944ad30c97bb42ff5da6033f
Jonas Muniz <ev.jonasmuniz@gmail.com>
2020-03-21 00:31:52 -0300

    Chained decisions

--- P03DecisionIf.py
+++ P03DecisionIf.py
@@ -1,13 +1,19 @@
 name=input("Enter the name: ")
 age=int(input("Enter the age: "))
 contagiousDisease=input("Suspected contagious disease? ").upper()
-if age >= 65 and contagiousDisease == "YES":
-    print("Refer the patient " + name + " to the yellow room - With priority")
-if age < 65 and contagiousDisease == "YES":
-    print("Refer the patient " + name + " to the yellow room - Without priority")
-if age >= 65 and contagiousDisease == "NO":
-    print("Refer the patient " + name + " to the white room - With priority")
-if age < 65 and contagiousDisease == "NO":
-    print("Refer the patient " + name + " to the white room - Without priority")
+if age >= 65:
+    print("Patient with priority")
+    if contagiousDisease == "YES":
+        print("Refer the patient " + name + " to the yellow room")
+    elif contagiousDisease == "NO":
+        print("Refer the patient " + name + " to the white room")
+    else:
+        print("Answer YES or NO to contagious disease")
 else:
-    print("Answer YES or NO to contagious disease")
+    print("Patient without priority")
+    if contagiousDisease == "YES":
+        print("Refer the patient " + name + " to the yellow room")
+    elif contagiousDisease == "NO":
+        print("Refer the patient " + name + " to the white room")
+    else:
+        print("Answer YES or NO to contagious disease")
