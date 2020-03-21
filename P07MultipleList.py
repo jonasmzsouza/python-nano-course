033a28fa7fc46e261fb856d2f9fed694a117f088
Jonas Muniz <ev.jonasmuniz@gmail.com>
2020-03-21 13:37:16 -0300

    Deleting list element

--- P07MultipleList.py
+++ P07MultipleList.py
@@ -9,7 +9,7 @@ while answer == "Y":
     serials.append(int(input("Serial Number: ")))
     departments.append(input("Department: "))
     answer = input("Enter \"Y\" to continue: ").upper()
-
+#DISPLAY
 for index in range(0,len(hardwares)):
     print("\n----------------------------------")
     print("Hardware...: ", (index+1))
@@ -19,18 +19,18 @@ for index in range(0,len(hardwares)):
     print("Serial.....: ", serials[index])
     print("Department.: ", departments[index])
     print("----------------------------------")
-
+#SEARCH
 search=input("\nEnter name of the hardware you want to search: ")
 for index in range(0,len(hardwares)):
     if search==hardwares[index]:
         print("\nPrice......: ", prices[index])
         print("Serial.....: ", serials[index])
-
+#ANSWER
 depreciation=input("\nDo you want to depreciate this hardware by 10%?").upper()
 while depreciation != "YES" and depreciation != "NO":
     print("Answer YES or NO to depreciate")
     depreciation=input("Do you want to depreciate this hardware by 10%?").upper()
-
+#CHANGE
 if depreciation == "YES":
     for index in range(0,len(hardwares)):
         if search==hardwares[index]:
@@ -39,4 +39,22 @@ if depreciation == "YES":
             print("New price:", prices[index])
 else:
     print("The price of the hardware \"" + search + "\" has not been depreciated.")
-
+#DELETE
+search=input("\nEnter name of the hardware you want to delete: ")
+for index in range(0,len(hardwares)):
+    if search==hardwares[index]:
+        del hardwares[index]
+        del prices[index]
+        del serials[index]
+        del departments[index]
+        break
+#NEW DISPLAY
+for index in range(0,len(hardwares)):
+    print("\n----------------------------------")
+    print("Hardware...: ", (index+1))
+    print("----------------------------------")
+    print("Description: ", hardwares[index])
+    print("Price......: ", prices[index])
+    print("Serial.....: ", serials[index])
+    print("Department.: ", departments[index])
+    print("----------------------------------")
