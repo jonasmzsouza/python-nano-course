25ecd92dbbed42953c84ba45e9e329ced4f91c80
Jonas Muniz <ev.jonasmuniz@gmail.com>
2020-03-25 15:27:25 -0300

    Tuple structure

--- P11Tuple.py
+++ P11Tuple.py
@@ -8,3 +8,16 @@ while answer=="Y":
 print("\nDisplaying IPs: ")
 for ip in ips.keys():
     print(ip[0]+"."+ip[1])
+
+print("\nDisplay machines with the same address: ")
+search=input("Enter the last two octets: ")
+print("Machines at the same address (different networks)")
+for ip,name in ips.items():
+    if(ip[1]==search):
+        print(name)
+
+print("\nThe machines that make up the same network: ")
+network=input("Enter the first two octets: ")
+for ip,name in ips.items():
+    if(ip[0]==network):
+        print(name)
