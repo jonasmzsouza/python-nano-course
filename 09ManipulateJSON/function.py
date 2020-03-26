0679df85f46de307ea5f77c0f7a5d48bf305057c
Jonas Muniz <ev.jonasmuniz@gmail.com>
2020-03-26 17:00:26 -0300

    Manipulating JSON

---
+++ 09ManipulateJSON/function.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+
+def callMenu():
+    choice = int(input("Enter:\n" +
+                       "<1> To register asset\n" +
+                       "<2> To view stored assets: "))
+    return choice
+
+
+def readFile(file):
+    if os.path.exists(file):
+        with open(file, "r") as file_json:
+            dictionary = json.load(file_json)
+    else:
+        dictionary = {}
+    return dictionary
+
+
+def writeFile(dictionary, file):
+    with open(file, "w") as file_json:
+        json.dump(dictionary, file_json)
+
+
+def register(dictionary, file):
+    answer = "Y"
+    while answer == "Y":
+        dictionary[input("Enter the patrimonial number: ")] = [input("\nEnter the date of the last update: "),
+                                                               input("Enter the description: "),
+                                                               input("Enter the department: ")]
+        answer = input("\nEnter <Y> to continue: ").upper()
+        writeFile(dictionary, file)
+    return "\nJSON generated!\n"
+
+
+def display(file):
+    dictionary = readFile(file)
+    for key, data in dictionary.items():
+        print("\nDate............: ", data[0])
+        print("Description.....: ", data[1])
+        print("Department......: ", data[2],"\n")
+
